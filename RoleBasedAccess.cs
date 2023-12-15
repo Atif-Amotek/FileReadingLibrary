ecb1f4d2df99ab4ee576e79a6b627b6a0c955209
Atif-Amotek <atif.khalil@amotek.be>
2023-12-15 14:56:46 +0100

    Added roles to the application
    Added 2 roles to the application Admin and User. Which based off of
    their roll they have access to the XML file or not. Also checking if the
    user has put in a valid input or not.

---
+++ RoleBasedAccess.cs
@@ -0,0 +1,31 @@
+﻿using FileReadingLibrary.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReadingLibrary
+{
+    public class RoleBasedAccess
+    {
+        private readonly string textFilePath = Resources.TextFile;
+        private readonly string xmlFilePath = Resources.TestXML;
+        public void IsUserInRole(string role)
+        {
+            if (role == "Admin")
+            {
+                TextFileReader textFileReader = new();
+                textFileReader.ReadFile(textFilePath);
+
+                XMLFileReader xmlFileReader = new();
+                xmlFileReader.ReadFile(xmlFilePath);
+            }
+            else if (role == "User")
+            {
+                TextFileReader textFileReader = new();
+                textFileReader.ReadFile(textFilePath);
+            }
+        }
+    }
+}
