5e9cf513e812b4c897042f48982261b16659dde2
Atif-Amotek <atif.khalil@amotek.be>
2023-12-15 11:16:59 +0100

    Added txt file and reading it

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FileReadingLibrary.Properties;
+using System;
+using System.IO;
+using System.Resources;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = Resources.TextFile;
+
+        if (filePath != null)
+        {
+            Console.WriteLine("File Content:");
+            Console.WriteLine(Resources.TextFile);
+        }
+        else
+        {
+            Console.WriteLine("File not found.");
+        }
+    }
+}
