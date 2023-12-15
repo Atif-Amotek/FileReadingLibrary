74246374893aa4cfad6b01418acf3f7b05185c33
Atif-Amotek <atif.khalil@amotek.be>
2023-12-15 11:34:07 +0100

    Refactored code into seperate classes

---
+++ TextFileReader.cs
@@ -0,0 +1,24 @@
+﻿using FileReadingLibrary.Properties;
+using System;
+using System.IO;
+using System.Resources;
+
+namespace FileReadingLibrary
+{
+    class TextFileReader: IFileReader
+    {
+
+        public void ReadFile(string textFilePath)
+        {
+            if (textFilePath != null)
+            {
+                Console.WriteLine("File Content:");
+                Console.WriteLine(Resources.TextFile);
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+    }
+}
---
+++ XMLFileReader.cs
@@ -0,0 +1,25 @@
+﻿using FileReadingLibrary.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReadingLibrary
+{
+    class XMLFileReader: IFileReader
+    {
+        public void ReadFile(string xmlFilePath)
+        {
+            if (xmlFilePath != null)
+            {
+                Console.WriteLine("XML Content:");
+                Console.WriteLine(Resources.TestXML);
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+    }
+}
